78afc103f3e58b9ef6e5a1baf30c8d2fcef646ad
Jure Cuhalev <jure@cuhalev.com>
2017-11-24 10:11:52 +0100

    Adds router scrolling

--- app/router.js
+++ app/router.js
@@ -1,9 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 import {htmlSafe} from '@ember/string';
+import RouterScroll from 'ember-router-scroll';
 
 
-const Router = EmberRouter.extend({
+const Router = EmberRouter.extend(RouterScroll, {
   location: config.locationType,
   rootURL: config.rootURL,
 });
