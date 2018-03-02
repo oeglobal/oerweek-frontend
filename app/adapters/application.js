9cb764d31f59460f8a2887f1830dfcbfb17ded62
Jure Cuhalev <jure@cuhalev.com>
2018-03-02 20:53:33 +0100

    Rehydrate ember-data from fastboot

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
 import DRFAdapter from './drf';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import CachedShoe   from 'ember-cached-shoe'
 
-export default DRFAdapter.extend(DataAdapterMixin, {
+export default DRFAdapter.extend(CachedShoe, DataAdapterMixin, {
   authorizer: 'authorizer:token',
 });
