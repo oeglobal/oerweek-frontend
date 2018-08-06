8761edcdf080a2d605af9e171ad37f5f0d80e8b9
Jure Cuhalev <jure@cuhalev.com>
2018-08-06 09:07:34 +0200

    Upgrade to Ember 3.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,26 @@ module.exports = {
   env: {
     browser: true
   },
-  rules: {}
+  rules: {
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };
