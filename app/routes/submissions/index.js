f9680915bb69f0f399f450949e31bf9c93a5d0e9
Jure Cuhalev <jure@cuhalev.com>
2018-08-07 16:24:08 +0200

    Improve CSS colors

--- app/routes/submissions/index.js
+++ app/routes/submissions/index.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return this.store.query('submission', {'page_size': 200});
+  }
 });
