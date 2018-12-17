450b4b1d58ab082a18c5bc66e0f1b1f6c6b5d8f5
Jure Cuhalev <jure@cuhalev.com>
2018-12-17 18:51:41 +0100

    Remove unused var

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 import {equal} from '@ember/object/computed';
 import {inject as service} from '@ember/service';
-import {computed} from '@ember/object';
 
 export default Controller.extend({
   session: service('session'),
--- config/environment.js
+++ config/environment.js
@@ -65,9 +65,9 @@ module.exports = function (environment) {
   if (environment === 'development') {
     // ENV.APP.API_HOST = 'http://localhost:8000';
 
-    ENV.APP.API_HOST = 'https://api.openeducationweek.org';
-    ENV['ember-simple-auth-token']['serverTokenEndpoint'] = 'https://api.openeducationweek.org/api-token-auth/';
-    ENV['ember-simple-auth-token']['serverTokenRefreshEndpoint'] = 'https://api.openeducationweek.org/api-token-refresh/';
+    // ENV.APP.API_HOST = 'https://api.openeducationweek.org';
+    // ENV['ember-simple-auth-token']['serverTokenEndpoint'] = 'https://api.openeducationweek.org/api-token-auth/';
+    // ENV['ember-simple-auth-token']['serverTokenRefreshEndpoint'] = 'https://api.openeducationweek.org/api-token-refresh/';
   }
 
   if (environment === 'test') {
