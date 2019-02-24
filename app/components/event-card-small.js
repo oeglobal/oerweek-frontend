fa414d689ccb07895bd894ddb071a3163aa3024b
Jure Cuhalev <jure@cuhalev.com>
2019-02-24 14:16:58 -0600

    launch

--- app/components/event-card-small.js
+++ app/components/event-card-small.js
@@ -8,14 +8,19 @@ export default Component.extend({
 
   timeDiff: computed('currentTime', function(){
     let currentTime = this.currentTime,
-      eventTime = moment(this.get('event.event_time'));
+      eventTime = moment(this.get('event.eventTime'));
 
     let timeDiff = currentTime.diff(eventTime, 'minutes');
+
     if (timeDiff > 0) {
       return `Started ${timeDiff} minutes ago`;
     } else{
       let minutes = Math.abs(timeDiff);
-      if (minutes > 60) {
+
+      if (minutes > 1440) {
+        let days = Math.round(minutes/60 * 10/24) / 10;
+        return `Begins in ${days} days`;
+      } else if (minutes > 60) {
         let hours = Math.round(minutes/60 * 10) / 10;
         return `Begins in ${hours} hours`;
       }
