1dcdea7ff0afa55ccd6f6d7655d41a473352a776
Jure Cuhalev <jure@cuhalev.com>
2020-02-24 16:18:08 +0100

    Adds OER to tags

--- app/utils/get-open-tags.js
+++ app/utils/get-open-tags.js
@@ -1,5 +1,5 @@
 export default function getOpenTags() {
   return ['Community and Technical Colleges', 'Open Access', 'Open Course (OCW / MOOC)', 'Open Culture', 'Open Data', 'Open Education Degree',
-    'Open Licenses', 'Open Pedagogy', 'Open Policy', 'Open Research', 'Open Repository', 'Open Science',
+    'Open Educational Resources', 'Open Licenses', 'Open Pedagogy', 'Open Policy', 'Open Research', 'Open Repository', 'Open Science',
     'Open Source Software', 'Open Textbook', 'Open Web', 'Other Open Content'];
 }
