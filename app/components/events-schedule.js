2893d00b2db436cde54e4398b7d3028f0d01175e
Jure Cuhalev <jure@cuhalev.com>
2020-03-03 15:51:04 +0100

    Update date

--- app/components/events-schedule.js
+++ app/components/events-schedule.js
@@ -6,7 +6,7 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   store: service(),
 
-  date: '2020-03-02',
+  date: '2020-03-03',
 
   isDateOther: equal('date', 'other'),
 
