4302d101b1b4a100f0adbf48f4f6f18a420cae9d
Jure Cuhalev <jure@cuhalev.com>
2021-01-19 15:51:07 +0100

    Fix review submissions

--- app/controllers/submissions/detail.js
+++ app/controllers/submissions/detail.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import {inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   user: service(),
@@ -7,7 +7,11 @@ export default Controller.extend({
   actions: {
     changeSubmissionStatus(model, postStatus) {
       model.set('postStatus', postStatus);
+
+      if (!model.image) {
+        model.set('image', '');
+      }
       model.save();
-    }
+    },
   },
 });
