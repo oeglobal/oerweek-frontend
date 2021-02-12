d80988c23ab386d7964ed90c4e9532734d610b4f
Jure Cuhalev <jure@cuhalev.com>
2021-02-12 11:55:45 +0100

    Implemented event listing

--- app/components/events-schedule.js
+++ app/components/events-schedule.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import moment from 'moment';
 
 export default class EventsScheduleComponent extends Component {
   @service store;
@@ -17,28 +18,47 @@ export default class EventsScheduleComponent extends Component {
     this.isDateAll = this.date === 'all';
   }
 
-  filteredLocalEvent() {
-    return this.store.query('event', {
-      event_type: 'local',
-      'page[size]': 99,
-      date: this.date,
-    });
-  }
+  dates = [
+    'all',
+    '2021-03-01',
+    '2021-03-02',
+    '2021-03-03',
+    '2021-03-04',
+    '2021-03-05',
+    'other',
+    'anytime',
+  ];
 
-  filteredOnlineEvent() {
-    return this.store.query('event', {
-      event_type: 'online',
-      'page[size]': 99,
-      date: this.date,
-    });
+  get filteredEvents() {
+    if (this.args.model) {
+      return this.args.model.filter((item) => {
+        if (this.date === 'anytime') {
+          return item.eventType === 'anytime';
+        }
+
+        if (this.date === 'other') {
+          return !moment(item.eventTime).isBetween('2021-03-01', '2021-03-06');
+        }
+
+        if (this.date === 'all') {
+          return (
+            item.event_type !== 'anytime' &&
+            moment(item.eventTime).isBetween('2021-03-01', '2021-03-06')
+          );
+        }
+
+        return moment(this.date).isSame(item.eventTime, 'day');
+      });
+    }
+
+    return [];
   }
 
   @action
   selectDate(selectedTab) {
-    const date = selectedTab.get('value');
-    this.date = date;
+    this.date = selectedTab;
 
-    this.isDateOther = date === 'other';
-    this.isDateAll = date === 'all';
+    this.isDateOther = this.date === 'other';
+    this.isDateAll = this.date === 'all';
   }
 }
--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -2,4 +2,9 @@ import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 
 @classic
-export default class ScheduleRoute extends Route {}
+export default class ScheduleRoute extends Route {
+  model() {
+    // noinspection JSCheckFunctionSignatures
+    return this.store.query('event', { 'page[size]': 999, year: 2021 });
+  }
+}
